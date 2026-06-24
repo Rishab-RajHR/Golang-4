9e4e55a7557e10e4d5d8828f81addf0d847a4776
Rishab Raj <rr0948555@gmail.com>
2026-06-24 17:47:17 +0530

    Slices

---
+++ Slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Slices are dynamic array
+// most used construct in go
+// + useful methods
+func main() {
+
+	// uninitialized slice is nil
+	var nums []int
+
+	fmt.Println(nums)
+
+}
