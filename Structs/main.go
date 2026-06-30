8913a79cf71ec34c026f969f4013a7bca74a5684
Rishab Raj <rr0948555@gmail.com>
2026-06-30 14:57:06 +0530

    Structs

--- Structs/main.go
+++ Structs/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+// Order struct
+
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time // nanosecond precision
+}
+
+// receiver type
+func (o *order) changeStatus(status string) {
+	o.status = status
+}
+
+func main() {
+	myOrder := order{
+		id:     "1",
+		amount: 50.00,
+		status: "received",
+	}
+
+	myOrder.changeStatus("confirmed")
+	fmt.Println(myOrder)
+
+	// myOrder.createdAt = time.Now()
+	// fmt.Println(myOrder.status)
+
+	// myOrder2 := order{
+	// 	id:        "2",
+	// 	amount:    100,
+	// 	status:    "delivered",
+	// 	createdAt: time.Now(),
+	// }
+
+	// myOrder.status = "paid"
+
+	// fmt.Println("Order struct", myOrder2)
+
+	// fmt.Println("Order struct", myOrder)
 }
