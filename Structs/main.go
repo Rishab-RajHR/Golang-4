52251cf6dae472d5ea935277f3d8b7d9bae32cad
Rishab Raj <rr0948555@gmail.com>
2026-06-30 16:37:51 +0530

    Structs

--- Structs/main.go
+++ Structs/main.go
@@ -1,33 +1,56 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 // Order struct
 
-type order struct {
-	id        string
-	amount    float32
-	status    string
-	createdAt time.Time // nanosecond precision
-}
+// type order struct {
+// 	id        string
+// 	amount    float32
+// 	status    string
+// 	createdAt time.Time // nanosecond precision
+// }
 
-// receiver type
-func (o *order) changeStatus(status string) {
-	o.status = status
-}
+// func newOrder(id string, amount float32, status string) *order {
+// 	myOrder := order{
+// 		id:     id,
+// 		amount: amount,
+// 		status: status,
+// 	}
+
+// 	return &myOrder
+// }
+
+// // receiver type
+// func (o *order) changeStatus(status string) {
+// 	o.status = status
+// }
+
+// func (o *order) getAmount() float32 {
+// 	return o.amount
+// }
 
 func main() {
-	myOrder := order{
-		id:     "1",
-		amount: 50.00,
-		status: "received",
-	}
 
-	myOrder.changeStatus("confirmed")
-	fmt.Println(myOrder)
+	language := struct {
+		name   string
+		isGood bool
+	}{"golang", true}
+
+	fmt.Println(language)
+
+	// myOrder := newOrder("1", 30.50, "received")
+	// fmt.Println(myOrder)
+	// If you don't set any field, default value is zero value
+	// int => 0, float => 0, string "", bool => false
+	// myOrder := order{
+	// 	id:     "1",
+	// 	amount: 50.00,
+	// 	status: "received",
+	// }
+
+	// myOrder.changeStatus("confirmed")
+	// fmt.Println(myOrder.getAmount())
 
 	// myOrder.createdAt = time.Now()
 	// fmt.Println(myOrder.status)
