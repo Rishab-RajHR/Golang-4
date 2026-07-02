4fa854c53fcf72fd5f316bca9d2f3942b98bc3d3
Rishab Raj <rr0948555@gmail.com>
2026-07-02 17:20:36 +0530

    Enums

--- Enums/main.go
+++ Enums/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+// Enumerated types
+
+type OrderStatus string
+
+const (
+	Received  OrderStatus = "received"
+	Confirmed             = "confirmed"
+	Prepared              = "prepared"
+	Delivered             = "delivered"
+)
+
+func changeOrderStatus(status OrderStatus) {
+	fmt.Println("Changing order status to", status)
+}
+
+func main() {
+	changeOrderStatus(Prepared)
 }
