e62e951b04ea2be6cb22ecbc85b93d317801b419
Rishab Raj <rr0948555@gmail.com>
2026-07-10 16:32:28 +0530

    Package

--- packages/main.go
+++ packages/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/packages/auth"
+	"github.com/packages/user"
+)
+
 func main() {
+	auth.LoginWithCredentials("Alex", "secret")
+	session := auth.GetSession()
+	fmt.Println("session", session)
+
+	user := user.User{
+		Email: "user@gmail.com",
+		Name:  "John Doe",
+	}
+
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
 
 }
